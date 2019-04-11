e0e8a6a4f422bdc668819efe6235555b9a101ce6
postoor <postoor@gmail.com>
2019-04-11 11:56:28 +0800

    Add Config and Script

---
+++ script/batch_user_add.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for user in $(cat user.list)
+do
+# Add new user
+adduser -m -c $user $user -s /bin/bash
+# Add user to sudo group
+usermod -a -G sudo $user
+# Default password: username
+echo -e "$user\n$user" | passwd $user
+# User must change password at first login
+passwd -e $user
+done%  
---
+++ script/find_and_count.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+var="$1";
+echo "$var";
+grep -r "$var" ./ | awk -F: '{print $1}' | uniq -c
+
+exit 0;
+
